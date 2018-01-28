daf54a09b155e627000d68687c9b13541af08de1
Craig <sgccarey@gmail.com>
2018-01-28 22:11:25 +0000

    02_standards: initial commit

---
+++ 02_standards/print_conf.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <errno.h>
+#include <stdio.h>
+#include <zconf.h>
+
+static void print_sysconf (char *mesg, int name)
+{
+    long val;
+
+    fputs(mesg, stdout);
+    errno = 0;
+    if ((val = sysconf(name)) < 0)
+    {
+        if (errno != 0)
+        {
+            if (errno == EINVAL)
+                fputs(" (not supported)\n", stdout);
+            else
+                fprintf(stderr, "sysconf error");
+        }
+        else
+        {
+            fputs(" (no limit)\n", stdout);
+        }
+    }
+    else
+    {
+        printf(" %ld\n", val);
+    }
+}
+
+static void print_pathconf (char *mesg, char *path, int name)
+{
+    long val;
+
+    fputs(mesg, stdout);
+    errno = 0;
+    if ((val = pathconf(path, name)) < 0)
+    {
+        if (errno != 0)
+        {
+            if (errno == EINVAL)
+                fputs(" (not supported)\n", stdout);
+            else
+                fprintf(stderr, "pathconf error, path = %s", path);
+        }
+        else
+        {
+            fputs(" (no limit)\n", stdout);
+        }
+    }
+    else
+    {
+        printf(" %ld\n", val);
+    }
+}
