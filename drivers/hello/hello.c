bf1dc66dd9a17d9f89376119d36e66c2bc1d8da1
Craig <sgccarey@gmail.com>
2018-04-27 14:34:31 +0100

    drivers: formatting

--- drivers/hello/hello.c
+++ drivers/hello/hello.c
@@ -7,18 +7,18 @@
  * that can display a message in the /var/log/kern.log file when the
  * module is loaded and removed. The module can accept an argument when
  * it is loaded -- the name, which appears in the kernel log files.
-*/
+ */
 
-#include <linux/init.h>      // macros to mark up functions e.g. __init
-#include <linux/module.h>    // core header for loading LKMs
-#include <linux/kernel.h>    // contains kernel types, macros, functions
+#include <linux/init.h>   // macros to mark up functions e.g. __init
+#include <linux/kernel.h> // contains kernel types, macros, functions
+#include <linux/module.h> // core header for loading LKMs
 
-MODULE_LICENSE("GPL");       // the license type (affects behavior)
-MODULE_AUTHOR("Derek Molloy");  // The author visible with modinfo
+MODULE_LICENSE("GPL");         // the license type (affects behavior)
+MODULE_AUTHOR("Derek Molloy"); // The author visible with modinfo
 MODULE_DESCRIPTION("A simple Linux LKM for the RPi."); // desc.
-MODULE_VERSION("0.1");       // the version of the module
+MODULE_VERSION("0.1"); // the version of the module
 
-static char *name = "world"; // example LKM argument default is "world"
+static char* name = "world"; // example LKM argument default is "world"
 // param description charp = char pointer, defaults to "world"
 module_param(name, charp, S_IRUGO); // S_IRUGO can be read/not changed
 MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
@@ -29,17 +29,21 @@ MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
  * a LKM) the function is only used at initialization time and that it
  * can be discarded and its memory freed up after that point.
  * @return returns 0 if successful  */
-static int __init helloERPi_init(void) {
-   printk(KERN_INFO "ERPi: Hello %s from the RPi LKM!\n", name);
-   return 0;
+static int __init
+helloERPi_init(void)
+{
+	printk(KERN_INFO "ERPi: Hello %s from the RPi LKM!\n", name);
+	return 0;
 }
 
 /** @brief The LKM cleanup function
  * Similar to the initialization function, it is static. The __exit
  * macro notifies that if this code is used for a built-in driver (not
  * a LKM) that this function is not required.    */
-static void __exit helloERPi_exit(void) {
-   printk(KERN_INFO "ERPi: Goodbye %s from the RPi LKM!\n", name);
+static void __exit
+helloERPi_exit(void)
+{
+	printk(KERN_INFO "ERPi: Goodbye %s from the RPi LKM!\n", name);
 }
 
 /** @brief A module must use the module_init() module_exit() macros from
